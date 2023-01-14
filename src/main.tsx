3bc03007d86daa39b8a478ef29dc12182df5d575
MichaelRayven <themikerayven@gmail.com>
2023-01-15 00:22:46 +0700

    Code formatting

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux/es/exports";
-import App from "./components/app/App";
-import { store } from "./app/store";
-import "./styles/index.scss";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { Provider } from "react-redux/es/exports"
+import App from "./components/app/App"
+import { store } from "./app/store"
+import "./styles/index.scss"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
@@ -11,4 +11,4 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       <App />
     </Provider>
   </React.StrictMode>
-);
+)
