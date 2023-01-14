24397b66df07d574aa911e34d31251c7cd5f561d
MichaelRayven <themikerayven@gmail.com>
2023-01-14 14:52:33 +0700

    Setup scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux/es/exports'
-import App from './App'
-import { store } from './app/store'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux/es/exports";
+import App from "./components/app/App";
+import { store } from "./app/store";
+import "./styles/index.scss";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
