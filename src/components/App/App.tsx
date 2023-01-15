b2a93043e76e4930e8723e862aed41d0bde39612
Michael Ray <86588495+MichaelRayven@users.noreply.github.com>
2023-01-15 05:27:35 +0000

    Cleaned up App styles, new folder structure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import Button from "../Button";
+import NavBar from "../NavBar";
+import "./App.scss";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+      <Button>Read More</Button>
+    </div>
+  );
+};
