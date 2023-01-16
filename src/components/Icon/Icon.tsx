3af2a31e20a20d52bdef67b5b5b73e5388afddef
Michael Ray <86588495+MichaelRayven@users.noreply.github.com>
2023-01-16 11:25:05 +0000

    Added style, className properties to Icon component

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React from "react"
+import "./Icon.scss"
 
 type Props = {
-  name: string;
-  style?: "outlined" | "rounded" | "sharp";
-  fill?: boolean;
-};
+  name: string
+  className?: string
+  style?: React.CSSProperties
+  iconStyle?: "outlined" | "rounded" | "sharp"
+  fill?: boolean
+}
 
-export const Icon = ({ name, style = "outlined", fill = false }: Props) => {
+export const Icon = ({
+  name,
+  style,
+  iconStyle = "rounded",
+  fill = false,
+  className = "",
+}: Props) => {
   return (
     <span
-      className={`material-symbols-${style} 
-      ${fill ? "icon_filled" : "icon"}`}
+      className={`material-symbols-${iconStyle} ${
+        fill ? "icon_filled" : "icon"
+      } ${className}`}
+      style={style}
     >
       {name}
     </span>
-  );
-};
+  )
+}
