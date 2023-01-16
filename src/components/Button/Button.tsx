bd7367b5cdb46f792bec1c4fa07ac22d40a686db
Michael Ray <86588495+MichaelRayven@users.noreply.github.com>
2023-01-16 11:26:53 +0000

    Changed Button component style, added className, style, onClick props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,17 @@
-import React from "react";
-import "./Button.scss";
+import React from "react"
+import "./Button.scss"
 
 type Props = {
-  children: React.ReactNode;
-};
+  children: React.ReactNode
+  className?: string
+  style?: React.CSSProperties
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
+}
 
-export const Button = (props: Props) => {
-  return <button className="button">{props.children}</button>;
-};
+export const Button = ({ children, className = "", onClick, style }: Props) => {
+  return (
+    <button onClick={onClick} className={`button ${className}`} style={style}>
+      {children}
+    </button>
+  )
+}
