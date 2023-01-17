43e677f6d764fd163ed469b1fd0cf9cd31a846c9
Michael Ray <86588495+MichaelRayven@users.noreply.github.com>
2023-01-17 15:13:55 +0000

    Used Header inside of app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,14 @@
-import Button from "../Button";
-import NavBar from "../NavBar";
-import "./App.scss";
+import Button from "../Button"
+import DropdownItem from "../DropdownItem"
+import Header from "../Header"
+import IconButton from "../IconButton"
+import NavBar from "../NavBar"
+import "./App.scss"
 
 export const App = () => {
   return (
-    <div className="App">
-      <NavBar></NavBar>
-      <Button>Read More</Button>
+    <div className="app">
+      <Header></Header>
     </div>
-  );
-};
+  )
+}
