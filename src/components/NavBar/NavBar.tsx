e656885bc9b4339f4f66e995fbef8a7bf9325cdf
Michael Ray <86588495+MichaelRayven@users.noreply.github.com>
2023-01-17 14:52:23 +0000

    Added className, style props, removed margin bottom

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,18 +1,21 @@
-import React from "react";
-import NavItem from "../NavItem";
-import "./NavBar.scss";
+import React from "react"
+import NavItem from "../NavItem"
+import "./NavBar.scss"
 
-type Props = {};
+type Props = {
+  className?: string
+  style?: React.CSSProperties
+}
 
-export const NavBar = (props: Props) => {
+export const NavBar = ({ className = "", style }: Props) => {
   return (
-    <nav className="nav">
+    <nav className={`nav ${className}`} style={style}>
       <ul className="nav__list">
-        <NavItem text={"Programming"}></NavItem>
+        <NavItem text={"Programming"} active></NavItem>
         <NavItem text={"Cooking"}></NavItem>
         <NavItem text={"Design"}></NavItem>
         <NavItem text={"Marketing"}></NavItem>
       </ul>
     </nav>
-  );
-};
+  )
+}
