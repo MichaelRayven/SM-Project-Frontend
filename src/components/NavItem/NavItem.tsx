19b38b51a5dbbff84e94bb88acca97991574ce01
Michael Ray <86588495+MichaelRayven@users.noreply.github.com>
2023-01-17 14:50:23 +0000

    Added hover, active styles

--- src/components/NavItem/NavItem.tsx
+++ src/components/NavItem/NavItem.tsx
@@ -1,18 +1,21 @@
-import React from "react";
-import "./NavItem.scss";
+import React from "react"
+import "./NavItem.scss"
 
 type Props = {
-  text: string;
+  text: string
   href?: string
-  active?: boolean;
-};
+  active?: boolean
+}
 
 export const NavItem = (props: Props) => {
   return (
     <li>
-      <a href={props.href} className={props.active ? "nav__link_active" : "nav__link"}>
+      <a
+        href={props.href}
+        className={props.active ? "nav__link_active" : "nav__link"}
+      >
         {props.text}
       </a>
     </li>
-  );
-};
+  )
+}
