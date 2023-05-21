a2ea0c1b709f619a5bc4232457aa0a3770ba2e1a
MichaelRayven <themikerayven@gmail.com>
2023-05-21 14:13:57 +0700

    Extracted logic from SignUp

--- src/views/pages/SignUp/SignUp.tsx
+++ src/views/pages/SignUp/SignUp.tsx
@@ -1,98 +1,28 @@
-import googleLogo from "@/assets/ic-google.svg"
-import twitterLogo from "@/assets/ic-twitter.svg"
-import vkLogo from "@/assets/ic-vk.svg"
-import facebookLogo from "@/assets/ic-facebook-square.svg"
-import githubLogo from "@/assets/ic-github.svg"
-import Button from "@/views/components/Button"
-import Input from "@/views/components/Input"
+import AuthForm from "@/views/components/AuthForm"
+import AuthLinks from "@/views/components/AuthMethods"
+import Card from "@/views/components/Card"
 import "./SignUp.scss"
-import HideableInput from "@/views/components/HideableInput"
-import { useForm } from "react-hook-form"
-import { useState } from "react"
-import { stringToRegExp } from "@/utility/utils"
+import { Link } from "react-router-dom"
 
 export const SignUp = () => {
-	const { register, handleSubmit, watch, formState: { errors } } = useForm()
-	const onSubmit = (data: any) => console.log(data)
-
 	return (
 		<div className="signup container">
-			<div className="signup__top">
-				<h6 className="signup__text">Sign up with socials:</h6>
-				<div className="signup__socials">
-					<img className="signup__icon" src={googleLogo} alt="" />
-					<img className="signup__icon" src={twitterLogo} alt="" />
-					<img className="signup__icon" src={vkLogo} alt="" />
-					<img className="signup__icon" src={facebookLogo} alt="" />
-					<img className="signup__icon" src={githubLogo} alt="" />
+			<Card>
+				<div className="signup__content signup__content_top">
+					<h6 className="signup__text">Sign up with socials:</h6>
+					<AuthLinks></AuthLinks>
+					<p className="highlight_inversion">or</p>
+					<h6 className="signup__text">Sign up using email:</h6>
+					<AuthForm variant="register" />
 				</div>
-				<p className="highlight_inversion">or</p>
-				<h6 className="signup__text">Sign up using email:</h6>
-				<form onSubmit={handleSubmit(onSubmit)} className="signup__form">
-					<Input 
-						{...register("username", {
-							required: "Username is required",
-							minLength: {
-								message: "Username needs to be at least 5 characters long",
-								value: 5
-							}
-						})} 
-						className="signup__field" 
-						label="Username"
-						error={errors.username !== undefined}
-						helperText={errors.username?.message as string}
-					/>
-					<Input 
-						{...register("email", {
-							required: "Email is required",
-							pattern: {
-								value: RegExp(/\w+@\w+\.\w{3}$/),
-								message: "Please enter a valid email, example: example@gmail.com"
-							}
-						})} 
-						className="signup__field" 
-						label="Email" 
-						type="email" 
-						error={errors.email !== undefined}
-						helperText={errors.email?.message as string}
-					/>
-					<HideableInput 
-						{...register("password", {
-							required: "Password is required",
-							minLength: {
-								message: "Password needs to be at least 8 characters long",
-								value: 8
-							}
-						})} 
-						className="signup__field" 
-						label="Password" 
-						error={errors.password !== undefined}
-						helperText={errors.password?.message as string}
-					/>
-					<HideableInput 
-						{...register("confirm_password", {
-							required: "Password confirmation is required",
-							minLength: {
-								message: "Password needs to be at least 8 characters long",
-								value: 8
-							},
-							validate: (val: string) => {
-								if (watch("password") != val) {
-									return "Your passwords do no match"
-								}
-							},
-						})} 
-						className="signup__field" 
-						label="Confirm Password" 
-						error={errors.confirm_password !== undefined}
-						helperText={errors.confirm_password?.message as string}
-					/>
-					<Button text="Sign up" type="submit" onClick={() => console.log(errors)} />
-				</form>
-			</div>
-			<div className="signup__bottom">
-				<h6 className="signup__text">Already registered? <a className="signup__link" href="">Sign into an existing account!</a></h6>
-			</div>
+			</Card>
+			<Card>
+				<div className="signup__content signup__content_bottom">
+					<h6 className="signup__text">
+						Already registered? <Link to="/signin" className="signup__link">Sign into an existing account!</Link>
+					</h6>
+				</div>
+			</Card>
 		</div>
 	)
 }
