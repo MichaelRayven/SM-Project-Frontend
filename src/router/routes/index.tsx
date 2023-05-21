e77aa8142814c5afdf2fd19a46d5ea26f757e8da
MichaelRayven <themikerayven@gmail.com>
2023-05-21 20:58:57 +0700

    Updated routes

--- src/router/routes/index.tsx
+++ src/router/routes/index.tsx
@@ -8,19 +8,20 @@ import Layout from "@/views/layouts/Default"
 // ** Default Route
 const defaultRoute = "/home"
 
-
 const Home = lazy(() => import("@/views/pages/Home"))
 const SignUp = lazy(() => import("@/views/pages/SignUp"))
+const SignIn = lazy(() => import("@/views/pages/SignIn"))
 const Search = lazy(() => import("@/views/pages/Search"))
-const ErrorPage = lazy(() => import("@/views/pages/Error"))
+const NotFoundPage = lazy(() => import("@/views/pages/Error"))
 
 const routes: RouteObject[] = createRoutesFromElements(
-	<Route path='/' element={<Layout />} errorElement={<ErrorPage />}>
+	<Route path='/' element={<Layout />} errorElement={<NotFoundPage />}>
 		<Route index path='home' element={<Home />} />
 		<Route path='search' element={<Search />} />
 		<Route path='signup' element={<SignUp />} />
+		<Route path='signin' element={<SignIn />} />
 		<Route path='/' element={<Navigate to={defaultRoute} replace />} />
-		<Route path='*' element={<ErrorPage />} />
+		<Route path='*' element={<NotFoundPage />} />
 	</Route>
 )
 
