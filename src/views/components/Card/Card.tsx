319813c82001c99847afafdfd58f6ff6b29b31c1
MichaelRayven <themikerayven@gmail.com>
2023-05-21 08:27:02 +0700

    Added a Card component

---
+++ src/views/components/Card/Card.tsx
@@ -0,0 +1,44 @@
+import "./Card.scss"
+
+type Props = {
+  borderRadius?: number
+  background?: string
+  elevation?: number
+  padding?: number
+  className?: string
+  style?: React.CSSProperties
+  children?: React.ReactNode
+}
+
+export const Card = ({
+	borderRadius = 8,
+	background = "rgb(var(--color-neutural))",
+	elevation = 8,
+	padding = 8,
+	className, 
+	style,
+	children
+}: Props) => {
+
+	const computeShadow = () => {
+		const elevationInRem = elevation / 16
+		return `${elevationInRem / 4}rem ${elevationInRem / 4}rem ${elevationInRem / 2}rem ${elevationInRem / 4}rem rgba(0, 0, 0, 0.2)`
+	}
+
+	return (
+		<div 
+			className={`card ${className}`}
+			style={{
+				borderRadius: `${borderRadius / 16}rem`,
+				boxShadow: computeShadow(),
+				padding: `${padding / 16}rem`,
+				background,
+				...style
+			}}
+		>
+			<div className="card__inner">
+				{children}
+			</div>
+		</div>
+	)
+}
