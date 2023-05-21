dcb380cc1d62938d34950fd36e1e8eb35191e60b
MichaelRayven <themikerayven@gmail.com>
2023-05-21 08:27:22 +0700

    Added type prop to Button

--- src/views/components/Button/Button.tsx
+++ src/views/components/Button/Button.tsx
@@ -3,13 +3,14 @@ import "./Button.scss"
 type Props = {
 	text: string,
 	icon?: string,
+	type?: "button" | "submit" | "reset"
 	className?: string,
 	onClick?: () => void
 }
 
-export const Button = ({text, icon, className, onClick}: Props) => {
+export const Button = ({text, icon, className, onClick, type = "button"}: Props) => {
 	return (
-		<button className={`button ${className || ""}`} type="button" onClick={onClick}>
+		<button className={`button ${className || ""}`} type={type} onClick={onClick}>
 			{icon && <span className="material-symbols-rounded icon">{icon}</span>}
 			{text}
 		</button>
